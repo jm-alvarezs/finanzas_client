824d1c4770a4811ba015bcfdb40675427ae5b8f1
Juan Manuel Alvarez <jm.alvarezs98@gmail.com2>
2019-12-06 14:26:50 -0600

    initial commit

---
+++ src/services/MovimientosService.js
@@ -0,0 +1,7 @@
+import api from "./api";
+
+const route = "/movimientos";
+
+export default {
+    getMovimientos: (idUsuario, fecha_inicio, fecha_fin) => api.get(`${route}/${idUsuario}?fechaInicio=${fecha_inicio}&fechaFin=${fecha_fin}`),
+}
